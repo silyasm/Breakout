ae9c8b0e4c1a994ec2bd1eb45fec4def2dce4854
Ilyas Munzir <ilyazmunzir@gmail.com>
2021-03-17 10:38:21 -0500

    Determine winners and losers

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -14,27 +14,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var brick = SKSpriteNode()
     var loseZone = SKSpriteNode()
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            paddle.position.x = location.x
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            paddle.position.x = location.x
-        }
-    }
-    
     override func didMove(to view: SKView) {
         //This stuff happens once (When app opens)
         physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         createBackground()
-        resetGame()
         makeLoseZone()
+        resetGame()
         kickBall()
     }
     
@@ -119,5 +105,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         loseZone.physicsBody?.isDynamic = false
         addChild(loseZone)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+            if contact.bodyA.node?.name == "brick" ||
+               contact.bodyB.node?.name == "brick" {
+                print("You win!")
+                brick.removeFromParent()
+                ball.removeFromParent()
+            }
+            if contact.bodyA.node?.name == "loseZone" ||
+               contact.bodyB.node?.name == "loseZone" {
+                print("You lose!")
+                ball.removeFromParent()
+            }
+        }
     
 }
